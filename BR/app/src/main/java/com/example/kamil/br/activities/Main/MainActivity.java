4d29627b13bafe1840e774753c199a9c026beefa
Kamil <kamil.machul@gmail.com>
2017-01-16 05:21:43 +0100

    Końcowy commit

--- BR/app/src/main/java/com/example/kamil/br/activities/Main/MainActivity.java
+++ BR/app/src/main/java/com/example/kamil/br/activities/Main/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.kamil.br.activities.main;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
